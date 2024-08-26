1fe23d7fd987f033895aaa36f76c9bab3e5df250
Jose Rodriguez <131327988+RodriguezJose92@users.noreply.github.com>
2024-08-26 16:20:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 class MudiExperience{
 
     constructor(){
-        this.color              = "#0071CE";
+        this.color              = "#0071CE"; 
         this.dataSever          = null;
         this.skuNumber          = null;
         this.fatherContainer    = null;
